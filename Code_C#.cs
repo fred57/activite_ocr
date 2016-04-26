9f58f3c08d1f628cd5915c62f9a8063ca53c0a3f
Fred <fred57@numericable.fr>
2016-04-26 16:50:53 +0200

    Ajout du fichier Code_C#.cs

---
+++ Code_C#.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPremiereApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* permet d'afficher du texte sur la console */
+            Console.WriteLine("Salut à toi en train de me lire !")
+        }
+    }
+}
